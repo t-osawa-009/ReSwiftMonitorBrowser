ea2cd33f7a3be0dbc84d2454fa51c3b6c06b04bb
t-osawa-009 <da87435@gmail.com>
2020-08-14 10:48:35 +0900

    Initial Commit

---
+++ ReSwiftMonitorBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReSwiftMonitorBrowser
+//
+//  Created by Takuya Ohsawa on 2020/08/14.
+//  Copyright © 2020 Takuya Ohsawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
